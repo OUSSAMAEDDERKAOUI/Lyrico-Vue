4088d669f68f902d2c6533f8e051b64e4b32b757
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-04-19 22:44:01 +0000

    la creation de template de la composant AudioPlayer.vue

---
+++ src/components/AudioPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="audio-player">
+    <audio ref="audioElement" :src="audioUrl" preload="none"></audio>
+    
+    <div class="player-controls">
+      <button class="play-button" @click="togglePlay">
+        {{ isPlaying ? '❚❚' : '▶' }}
+      </button>
+      
+      <div class="progress-container">
+        <div class="progress-bar" :style="{ width: `${progress}%` }"></div>
+      </div>
+      
+      <div class="time-display">{{ formatTime(currentTime) }} / {{ formatTime(duration) }}</div>
+    </div>
+  </div>
+</template>
+
+
