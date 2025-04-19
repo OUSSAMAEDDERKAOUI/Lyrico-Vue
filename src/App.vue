b66d11db3843a9d61276ccf7b5ac598c366aaeab
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2025-04-19 22:46:41 +0000

    Gestion de l'état dans App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="app-container">
+    <header>
+      <h1>Music Lyrics Finder</h1>
+      <SearchBar @search="searchSongs" :isLoading="isLoading" />
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <main>
+      <div v-if="error" class="error-message">
+        {{ error }}
+      </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+      <div v-if="isLoading" class="loading">
+        Recherche en cours...
+      </div>
+
+      <div v-else-if="searchPerformed && !searchResults.length" class="no-results">
+        Aucun résultat trouvé pour "{{ lastSearchTerm }}"
+      </div>
+
+      <ResultsList 
+        v-else-if="searchResults.length > 0" 
+        :results="searchResults" 
+        @select-song="selectSong" 
+      />
+
+      <SongDetails 
+        v-if="selectedSong" 
+        :song="selectedSong" 
+        :lyrics="lyrics" 
+        :isLoadingLyrics="isLoadingLyrics"
+        @close="closeSongDetails" 
+      />
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
